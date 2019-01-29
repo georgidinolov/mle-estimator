596b6136b44b3b4c6478e74c836e3a24b07c5d88
Georgi Svetoslavov Dinolov <gdinolov@grape.soe.ucsc.edu>
2019-01-29 11:04:32 -0800

    mle with matching

--- 2DMLEFiniteElement.cpp
+++ 2DMLEFiniteElement.cpp
@@ -5,6 +5,7 @@
 #include <omp.h>
 #include <string>
 #include "2DMLEFiniteElement.hpp"
+#include "src/gaussian-interpolator/GaussianInterpolator.hpp"
 
 namespace{
   inline double logit(double r) {
@@ -26,6 +27,99 @@ namespace{
   inline double round_32(double input) {
     double divisor = 1.0/32.0;
     return divisor * std::round(input/divisor);
+  }
+
+  gsl_vector* find_weights(const std::vector<double>& ys,
+			   const std::vector<double>& t_tildes,
+			   const std::vector<double>& alphas,
+			   const std::vector<double>& lambdas) {
+
+    // This is the design matrix X with dimensions m =
+    // size(t_tildes) x (n = size(lambdas) x o = size(alphas))
+    unsigned oo = alphas.size();
+    unsigned nn = lambdas.size();
+    unsigned mm = ys.size();
+
+    double y_array [mm];
+    double X_array [mm*nn*oo];
+
+    // filling out the design matrix
+    gsl_matrix_view X_view = gsl_matrix_view_array(X_array, mm, nn*oo);
+    gsl_vector_view y_view = gsl_vector_view_array(y_array, mm);
+
+    for (unsigned ii=0; ii<mm; ++ii) {
+      gsl_vector_set(&y_view.vector, ii, exp(log(ys[ii]) -lambdas[0]*t_tildes[ii]) );
+
+      for (unsigned jj=0; jj<nn; ++jj) {
+
+	for (unsigned kk=0; kk<oo; ++kk) {
+	  gsl_matrix_set(&X_view.matrix, ii, jj,
+			 exp((lambdas[jj] - lambdas[0])*t_tildes[ii]) *
+			 std::pow(t_tildes[ii], alphas[kk]));
+	}
+      }
+    }
+    const gsl_matrix* X = &X_view.matrix;
+
+    double work_array [(nn*oo)*(nn*oo)];
+    gsl_matrix_view work_view = gsl_matrix_view_array(work_array, nn*oo, nn*oo);
+
+    // X^T * X
+    gsl_blas_dgemm(CblasTrans, CblasNoTrans, 1.0, X, X, 0.0, &work_view.matrix);
+
+    // b = X^T y
+    double b_array [nn*oo];
+    gsl_vector_view b_view = gsl_vector_view_array(b_array, nn*oo);
+    gsl_blas_dgemv(CblasTrans, 1.0, X, &y_view.vector, 0.0, &b_view.vector);
+
+    // b = (X^T * X) weights
+    gsl_vector* weights = gsl_vector_alloc(nn*oo);
+    gsl_permutation* p = gsl_permutation_alloc(nn*oo);
+    int s = 0;
+
+    gsl_linalg_LU_decomp(&work_view.matrix, p, &s);
+    gsl_linalg_LU_solve(&work_view.matrix, p, &b_view.vector, weights);
+
+    gsl_permutation_free(p);
+
+    return (weights);
+  }
+
+  double find_max(const gsl_vector* weights, 
+		  const std::vector<double>& lambdas, 
+		  const std::vector<double>& alphas,
+		  const std::vector<double>& t_tildes,
+		  double small_t) {
+    // Find max according to the approximating solution start
+    double t_min = 0.0;
+    double t_max = 0.0;
+    double w1 = gsl_vector_get(weights, 0);
+    double w2 = gsl_vector_get(weights, 1);
+    if ((w1 > 0.0) & (w2 > 0.0)) {
+      t_min = small_t;
+      t_max = t_tildes[t_tildes.size()-1];
+    } else if ((w1 > 0.0) & (w2 < 0.0)) {
+      t_min = -1.0*log(std::abs(w1/w2))/std::abs(lambdas[1]-lambdas[0]);
+      t_max = t_tildes[t_tildes.size()-1];
+    } else if ((w1 < 0.0) & (w2 > 0.0)) {
+      t_min = small_t;
+      t_max = -1.0*log(std::abs(w1/w2))/std::abs(lambdas[1]-lambdas[0]);
+    } else {
+      t_min = small_t;
+      t_max = t_tildes[t_tildes.size()-1];
+    }
+
+    std::vector<double> ts(100);
+    unsigned nn = 0;
+    double dt = (t_max - t_min)/99;
+    std::generate(ts.begin(), ts.end(), [&] () mutable { double out = t_min + dt*nn; nn++; return out; });
+    std::sort(ts.begin(), ts.end(), [&lambdas, &alphas, &w1, &w2] (double t1, double t2)->bool {
+	double Delta = (lambdas[1]-lambdas[0]);
+	double d1 = std::abs(lambdas[0] + alphas[0]/t1 + Delta*w2*exp(Delta*t1)/(w1 + w2*exp(Delta*t1)));
+	double d2 = std::abs(lambdas[0] + alphas[0]/t2 + Delta*w2*exp(Delta*t2)/(w1 + w2*exp(Delta*t2)));
+	return (d1 < d2); });
+    t_max = (ts[0] + ts[1])/2.0; // this is the maximum point for the approximate solution
+    return t_max;
   }
 }
 
@@ -76,8 +170,8 @@ TwoDMLEFiniteElement(std::string data_file_dir,
 
   // THREADPRIVATE BASES START
 #pragma omp threadprivate(private_bases_)
-  unsigned i;
-#pragma omp parallel default(none) private(i) shared(bases)
+
+#pragma omp parallel default(none) shared(bases)
   {
     private_bases_ = new std::vector<BivariateGaussianKernelBasis> (1);
     (*private_bases_)[0] = bases[0];
@@ -121,6 +215,10 @@ TwoDMLEFiniteElement(std::string data_file_dir,
       // third value on the row is rho
       std::getline(data_file, value, ',');
       double rho = std::stod(value);
+      std::cout << "sigma_x=" << sigma_x
+		<< "; sigma_y=" << sigma_y
+		<< "; rho=" << rho
+		<< std::endl;
 
       // fourth value on the row is x_0
       std::getline(data_file, value, ',');
@@ -210,8 +308,7 @@ TwoDMLEFiniteElement(std::string data_file_dir,
 
   // THREADPRIVATE BASES START
 #pragma omp threadprivate(private_bases_)
-  unsigned i;
-#pragma omp parallel default(none) private(i) shared(bases)
+#pragma omp parallel default(none) shared(bases)
   {
     private_bases_ = new std::vector<BivariateGaussianKernelBasis> (1);
     (*private_bases_)[0] = bases[0];
@@ -255,6 +352,10 @@ TwoDMLEFiniteElement(std::string data_file_dir,
       // third value on the row is rho
       std::getline(data_file, value, ',');
       double rho = std::stod(value);
+      std::cout << "sigma_x=" << sigma_x
+		<< "; sigma_y=" << sigma_y
+		<< "; rho=" << rho
+		<< std::endl;
 
       // fourth value on the row is x_0
       std::getline(data_file, value, ',');
@@ -406,6 +507,10 @@ TwoDMLEFiniteElement(std::string data_file_dir,
       // third value on the row is rho
       std::getline(data_file, value, ',');
       double rho = std::stod(value);
+      std::cout << "sigma_x=" << sigma_x
+		<< "; sigma_y=" << sigma_y
+		<< "; rho=" << rho
+		<< std::endl;
 
       // fourth value on the row is x_0
       std::getline(data_file, value, ',');
@@ -531,6 +636,10 @@ void TwoDMLEFiniteElement::set_data_file(std::string data_file_dir)
       // third value on the row is rho
       std::getline(data_file, value, ',');
       double rho = std::stod(value);
+      std::cout << "sigma_x=" << sigma_x
+		<< "; sigma_y=" << sigma_y
+		<< "; rho=" << rho
+		<< std::endl;
 
       // fourth value on the row is x_0
       std::getline(data_file, value, ',');
@@ -726,20 +835,37 @@ negative_log_likelihood_parallel(int order,
 #pragma omp for
     for (i=0; i<N; ++i) {
       if (i==0) { printf("Thread %d with address %p\n", omp_get_thread_num(), private_bases_); }
-      
+
       double Lx = data[i].get_b() - data[i].get_a();
       double Ly = data[i].get_d() - data[i].get_c();
-      
-      double x_0 = (data[i].get_x_0() - data[i].get_a())/Lx;
-      double y_0 = (data[i].get_y_0() - data[i].get_c())/Ly;
 
-      double x_T = (data[i].get_x_T() - data[i].get_a())/Lx;
-      double y_T = (data[i].get_y_T() - data[i].get_c())/Ly;
+      double x_0_tilde = (data[i].get_x_0() - data[i].get_a())/Lx;
+      double y_0_tilde = (data[i].get_y_0() - data[i].get_c())/Ly;
 
-      double sigma_x_star = sigma_x/Lx;
-      double sigma_y_star = sigma_y/Ly;
+      double x_t_tilde = (data[i].get_x_T() - data[i].get_a())/Lx;
+      double y_t_tilde = (data[i].get_y_T() - data[i].get_c())/Ly;
 
+      double tau_x = sigma_x/Lx;
+      double tau_y = sigma_y/Ly;
+
+      double sigma_y_tilde = tau_y/tau_x;
       double rho_star = rho;
+
+      double t_tilde = t*tau_x*tau_x;
+
+
+      if (tau_x < tau_y) {
+	x_t_tilde = (data[i].get_y_T() - data[i].get_c())/Ly;
+	y_t_tilde = (data[i].get_x_T() - data[i].get_a())/Lx;
+	//
+	x_0_tilde = (data[i].get_y_0() - data[i].get_c())/Ly;
+	y_0_tilde = (data[i].get_x_0() - data[i].get_a())/Lx;
+	//
+	sigma_y_tilde = tau_x/tau_y;
+	//
+	t_tilde = t*std::pow(tau_y, 2);
+      }
+
 
       // flipping x-axis when rho<0
       //
@@ -749,36 +875,283 @@ negative_log_likelihood_parallel(int order,
       // a -> -a
       // b -> -b
       if (std::signbit(rho)) {
-	x_0 = (-data[i].get_x_0() - -1.0*data[i].get_b())/Lx;
-	x_T = (-data[i].get_x_T() - -1.0*data[i].get_b())/Lx;
+	x_0_tilde = (-data[i].get_x_0() - -1.0*data[i].get_b())/Lx;
+	x_t_tilde = (-data[i].get_x_T() - -1.0*data[i].get_b())/Lx;
 
 	rho_star = -1.0*rho_star;
       }
 
+      // printf("t_tilde = %f\n", t_tilde);
       BivariateSolver solver = BivariateSolver(&(*private_bases_)[k],
-					       sigma_x_star,
-					       sigma_y_star,
+					       1.0,
+					       sigma_y_tilde,
 					       rho_star,
 					       0.0, // a
-					       x_0, // x0
+					       x_0_tilde, // x0
 					       1.0, // b
 					       0.0, // c
-					       y_0, // y0
+					       y_0_tilde, // y0
 					       1.0, // d
-					       t,
+					       t_tilde,
 					       dx);
-      gsl_vector_set(input, 0, x_T);
-      gsl_vector_set(input, 1, y_T);
-      double likelihood = solver.numerical_likelihood(input,
-						      dx_likelihood)/
-	(Lx*Ly);
-      
+
+      std::vector<double> eigenvalues (5);
+      eigenvalues[0] = gsl_vector_get(solver.get_evals(), 0);
+      eigenvalues[1] = gsl_vector_get(solver.get_evals(), 1);
+      eigenvalues[2] = gsl_vector_get(solver.get_evals(), 2);
+      eigenvalues[3] = gsl_vector_get(solver.get_evals(), 3);
+      eigenvalues[4] = gsl_vector_get(solver.get_evals(), 4);
+      gsl_vector_set(input, 0, x_t_tilde);
+      gsl_vector_set(input, 1, y_t_tilde);
+
+      double likelihood = 0.0;
+
+      if (t_tilde >= 0.30) {
+	likelihood = solver.numerical_likelihood(input,
+						 dx_likelihood)/
+	  (Lx*Ly);
+      } else {
+	likelihood = std::numeric_limits<double>::infinity();
+      }
+
       // printf("Thread %d: likelihood = %f\n", omp_get_thread_num(), likelihood);
-      if (!std::signbit(likelihood)) {
-	neg_log_likelihoods[i] = -log(likelihood);
+      if (!std::signbit(likelihood) &&
+	  (std::abs(likelihood) < std::numeric_limits<double>::infinity()) &&
+	  !std::isnan(likelihood) &&
+	  t_tilde >= 0.30)
+	{
+	neg_log_likelihoods[i] = -std::log(likelihood);
 	bad_likelihoods_indicators[i] = 0;
       } else {
-	neg_log_likelihoods[i] = -log(1e-16);
+	std::vector<BivariateImageWithTime> small_positions =
+	  solver.small_t_image_positions_type_41_symmetric(false);
+	double small_t = small_positions[0].get_t();
+	double rho_for_small_t = rho_star;
+
+	// for some configurations, the small-time solution doesn't
+	// work, in which case small-t is negative. Shrink rho
+	// sufficiently s.t. the small-t solution works.  It's
+	// always guaranteed to work for sufficiently small rho.
+	while (std::signbit(small_t)) {
+	  rho_for_small_t = rho_for_small_t * 0.95;
+	  solver.set_diffusion_parameters_and_data_small_t(1.0,
+							   sigma_y_tilde,
+							   rho_for_small_t,
+							   t_tilde,
+							   0.0,
+							   x_0_tilde,
+							   1.0,
+							   0.0,
+							   y_0_tilde,
+							   1.0);
+	  small_positions = solver.small_t_image_positions_type_41_symmetric(false);
+	  small_t = small_positions[0].get_t();
+	}
+
+	// MATCHING CONSTANTS START
+	unsigned number_big_t_points = 2;
+	double alpha = 4.0;
+
+	// ------------------ //
+	// std::vector<double>::const_iterator first_lambda = eigenvalues.begin();
+	// std::vector<double>::const_iterator last_lambda = eigenvalues.begin() + 1;
+	std::vector<double> lambdas = std::vector<double> {eigenvalues[0],
+							   eigenvalues[1]};
+	// ------------------ //
+
+	std::vector<double> t_tildes_small = std::vector<double> {small_t};
+	std::vector<double> alphas = std::vector<double> {alpha};
+
+	std::vector<double> ys_small (t_tildes_small.size());
+	std::vector<double> log_ys_small (t_tildes_small.size());
+
+	std::generate(log_ys_small.begin(), log_ys_small.end(),
+		      [n = 0,
+		       &solver,
+		       input,
+		       &t_tildes_small] () mutable { 
+			double out = solver.
+			  likelihood_small_t_41_truncated_symmetric(input,
+								    t_tildes_small[n],
+								    1e-5);
+			n++;
+			return out;});
+
+	std::generate(ys_small.begin(), ys_small.end(),
+		      [n = 0,
+		       &log_ys_small] () mutable { double out = exp(log_ys_small[n]);
+			n++;
+			return out;});	
+
+	std::vector<double> t_tildes (0);
+	std::vector<double> ys (0);
+	std::vector<double> log_ys (0);
+
+	double t_tilde_2 = 0.80;
+	while (number_big_t_points > 0) {
+	  solver.set_diffusion_parameters_and_data_small_t(1.0,
+							   sigma_y_tilde,
+							   rho_for_small_t,
+							   t_tilde_2,
+							   0.0,
+							   x_0_tilde,
+							   1.0,
+							   0.0,
+							   y_0_tilde,
+							   1.0);
+
+	  double y2 = solver.numerical_likelihood(input,
+						  dx_likelihood);
+
+	  while (std::isnan(std::log(y2))) {
+
+	    if (t_tilde_2 <= 4) {
+	      t_tilde_2 = t_tilde_2 + 1.0;
+	    } else {
+	      t_tilde_2 = t_tilde_2 + 20.0;
+	    }
+	    solver.set_diffusion_parameters_and_data_small_t(1.0,
+							     sigma_y_tilde,
+							     rho_for_small_t,
+							     t_tilde_2,
+							     0.0,
+							     x_0_tilde,
+							     1.0,
+							     0.0,
+							     y_0_tilde,
+							     1.0);
+	    y2 = solver.numerical_likelihood(input,
+					     dx_likelihood);
+
+	    printf("Encountered big t with bad likelihood, trying t_tilde = %f, t_tilde_2 = %f w/ like=%f\n",
+		   t_tilde,
+		   t_tilde_2,
+		   y2);
+	  }
+
+	  t_tildes.push_back(t_tilde_2);
+	  ys.push_back(y2);
+
+	  t_tilde_2 = t_tilde_2 + 2.0;
+	  number_big_t_points--;
+	}
+	
+	gsl_vector* weights = find_weights(ys, t_tildes, alphas, lambdas);
+	double t_max = find_max(weights, lambdas, alphas, t_tildes, small_t);
+	solver.set_diffusion_parameters_and_data_small_t(1.0,
+							 sigma_y_tilde,
+							 rho_for_small_t,
+							 t_max,
+							 0.0,
+							 x_0_tilde,
+							 1.0,
+							 0.0,
+							 y_0_tilde,
+							 1.0);
+
+	double galerkin_like = solver.numerical_likelihood(input,
+							   dx_likelihood);
+
+	double approx_log_like = alphas[0]*log(t_max) + lambdas[0]*t_max + 
+	  log(gsl_vector_get(weights, 0) + gsl_vector_get(weights, 1)*exp((lambdas[1]-lambdas[0]) * t_max));
+	
+	if (!std::isnan(log(galerkin_like)) &
+	    (log(galerkin_like) > approx_log_like) ) {
+
+	  ys[0] = galerkin_like;
+	  t_tildes[0] = t_max;
+
+	  gsl_vector_free(weights);
+	  weights = find_weights(ys, t_tildes, alphas, lambdas);
+	  t_max = find_max(weights, lambdas, alphas, t_tildes, small_t);
+	}
+
+	// RHS matching
+	double w1 = gsl_vector_get(weights, 0);
+	double w2 = gsl_vector_get(weights, 1);
+	double Delta = lambdas[1]-lambdas[0];
+	double function_val = alphas[0]*log(t_max) + lambdas[0]*t_max + log(w1 + w2*exp(Delta*t_max));
+	double first_deriv =  lambdas[0] + alphas[0]/t_max + Delta*w2*exp(Delta*t_max)/(w1 + w2*exp(Delta*t_max));
+	double second_deriv = -alphas[0]/(t_max*t_max) + 
+	  Delta*Delta*w2*exp(Delta*t_max)/(w1 + w2*exp(Delta*t_max)) -
+	  Delta*Delta*w2*w2*exp(2*Delta*t_max)/std::pow(w1 + w2*exp(Delta*t_max), 2.0);
+	double deriv_matrix_array [4] = {-1.0/t_max, 1.0/(t_max*t_max), 1.0/(t_max*t_max), -2.0/(t_max*t_max*t_max)};
+	gsl_matrix_view deriv_matrix_view = gsl_matrix_view_array(deriv_matrix_array, 2,2);
+	
+	gsl_vector* gamma_beta_t_max = gsl_vector_alloc(2);
+	gsl_vector* b_vector = gsl_vector_alloc(2);
+	gsl_vector_set(b_vector, 0, first_deriv);
+	gsl_vector_set(b_vector, 1, second_deriv);
+	gsl_permutation* p = gsl_permutation_alloc(2);
+	int s = 0;
+
+	gsl_linalg_LU_decomp(&deriv_matrix_view.matrix, p, &s);
+	gsl_linalg_LU_solve(&deriv_matrix_view.matrix, p, b_vector, gamma_beta_t_max);
+
+	gsl_permutation_free(p);
+	gsl_vector_free(b_vector);
+
+	double gamma_t_max = gsl_vector_get(gamma_beta_t_max, 0);
+	double beta_t_max = gsl_vector_get(gamma_beta_t_max, 1);
+	double log_omega_t_max = function_val + gamma_t_max*log(t_max) + beta_t_max/t_max;
+	gsl_vector_free(gamma_beta_t_max);
+
+	// LHS matching 
+	double log_omega_t_small = -1.0*log(M_PI*std::sqrt(2)) - 
+	  4.5*(log(2.0) +
+	       2.0*log(sigma_y_tilde) +
+	       log(1-rho_for_small_t*rho_for_small_t));
+
+	std::vector<double> dPdaxs = solver.dPdax(input, dx_likelihood);
+	std::vector<double> dPdbxs = solver.dPdbx(input, dx_likelihood);
+	std::vector<double> dPdays = solver.dPday(input, dx_likelihood);
+	std::vector<double> dPdbys = solver.dPdby(input, dx_likelihood);
+	
+	std::vector<double> betas_t_small = std::vector<double> (4);
+	unsigned nn = 0;
+	std::generate(betas_t_small.begin(), 
+		      betas_t_small.end(), 
+		      [&] () mutable { 
+			double beta_t_small = 1.0/(2.0*
+						   sigma_y_tilde*
+						   sigma_y_tilde*
+						   (1.0 - rho*rho))*
+			  dPdaxs[nn]*dPdbxs[nn]*dPdays[nn]*dPdbys[nn];
+			nn++;
+			return beta_t_small;
+		      });
+
+	std::sort(betas_t_small.begin(), betas_t_small.end());
+
+	double beta_t_small = betas_t_small[3];
+	double gamma_t_small = 4.5;
+
+	double k = 100;
+	double log_omega = log_omega_t_small*exp(-k*(t_tilde-small_t)) +
+	  log_omega_t_max*(1 - exp(-k*(t_tilde-small_t)));
+	double gamma = gamma_t_small*exp(-k*(t_tilde-small_t)) +
+	  gamma_t_max*(1 - exp(-k*(t_tilde-small_t)));
+	double beta = beta_t_small*exp(-k*(t_tilde-small_t)) +
+	  beta_t_max*(1 - exp(-k*(t_tilde-small_t)));
+	// MATCH CONSTANTS END
+
+	double matched_sol = log_omega - gamma*log(t_tilde) - beta/t_tilde;
+	printf("lambdas[0] = %f, Delta = %f, alphas[0] = %f, t_max = %f, w1 = %f, w2 = %f, function_val = %f, log_omega_t_small = %f, log_omega_t_max = %f, log_omega = %f, gamma = %f, beta = %f, matched_sol = %f\n", 
+	       lambdas[0],
+	       Delta,
+	       alphas[0],
+	       t_max,
+	       w1, w2,
+	       function_val,
+	       log_omega_t_small,
+	       log_omega_t_max,
+	       log_omega,
+	       gamma,
+	       beta,
+	       matched_sol);
+	double log_likelihood = matched_sol;
+
+	neg_log_likelihoods[i] = -log_likelihood;
 	bad_likelihoods_indicators[i] = 1;
       }
     }
@@ -792,7 +1165,7 @@ negative_log_likelihood_parallel(int order,
   for (unsigned i=0; i<neg_log_likelihoods.size(); ++i) {
     sum_of_elements_parallel = sum_of_elements_parallel +
       neg_log_likelihoods[i];
-    
+
     std::cout << neg_log_likelihoods[i] << " ";
 
     number_bad_likelihoods = number_bad_likelihoods +
